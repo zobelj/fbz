9252831675c399dd8b94ae02807bb537b55cb735
Joe Zobel <zobelj@purdue.edu>
2021-12-03 18:38:42 -0500

    Still committing

--- fbz/settings.py
+++ fbz/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if "DYNO" in os.environ:
     STATIC_ROOT = 'static'
     ALLOWED_HOSTS = ['immense-dawn-61139.herokuapp.com']
+
+django_heroku.settings(locals())
